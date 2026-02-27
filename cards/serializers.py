dfe1e67080d4480a0c65eb0bee04b5f71f59e2da
Harshit <harshit392615@gmail.com>
2026-02-27 12:27:48 +0530

    fixed card delete

--- cards/serializers.py
+++ cards/serializers.py
@@ -5,7 +5,8 @@ class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = (
-            'title','description','status','due_date','assignee','created_at','slug'
+
+            'id','title','description','status','due_date','assignee','created_at','slug'
         )
 class CardCreateSerializer(serializers.ModelSerializer):
     class Meta:

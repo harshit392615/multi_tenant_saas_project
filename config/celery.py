4f15626a2788e0d82c4745d0e47eb8bfce7ffa4e
Harshit <harshit392615@gmail.com>
2026-03-02 23:10:34 +0530

    fixing ssly in celery worker

--- config/celery.py
+++ config/celery.py
@@ -1,3 +1,4 @@
+import ssl
 import os
 from celery import Celery
 from django.conf import settings
@@ -5,7 +6,6 @@ from django.conf import settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
 
 app = Celery("config")
-
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
@@ -15,8 +15,11 @@ backend_url = settings.CELERY_RESULT_BACKEND
 app.conf.broker_url = broker_url
 app.conf.result_backend = backend_url
 
-if (broker_url and broker_url.startswith("rediss://")) or \
-   (backend_url and backend_url.startswith("rediss://")):
-    ssl_options = {"ssl_cert_reqs": "CERT_NONE"}  # Use CERT_REQUIRED for production
-    app.conf.broker_transport_options = ssl_options
+if (broker_url and broker_url.startswith("rediss://")) or (backend_url and backend_url.startswith("rediss://")):
+
+    ssl_options = {
+        "ssl_cert_reqs": ssl.CERT_NONE
+    }
+
+    app.conf.broker_use_ssl = ssl_options
     app.conf.redis_backend_use_ssl = ssl_options
